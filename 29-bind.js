a800a54da4ea3feca9d271df9140afb3d4c7a277
luong <vanluongnbbl@gmail.com>
2022-04-04 02:34:03 +0700

    update bind method

---
+++ 29-bind.js
@@ -0,0 +1,86 @@
+/*
+    Bind
+
+    1. Phương thức bind sẽ trả về một hàm mới
+    2. Có thể nhận các đối số như hàm ban đầu
+*/ 
+
+
+this.firstName = "Minh"
+this.lastName = "Thu"
+
+const teacher = {
+    firstName : "Van",
+    lastName: "Luong",
+    getFullName(age) {
+        return `${this.firstName} ${this.lastName} - ${age}`
+    }
+}
+
+const student = {
+    firstName: "Van",
+    lastName: "Nam"
+}
+
+// console.log(teacher.getFullName());
+const getTeacherName = teacher.getFullName.bind(student)
+
+// console.log(getTeacherName(20));
+
+// ----------------------------------------------------------------
+
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const appCar = (() => {
+    const cars = ['BMW']
+    const root = $('#ulBind')
+    const input = $('#inputBind')
+    const submit = $('#addBind')
+
+
+    return {
+        add(car) {
+            cars.push(car)
+        },
+        delete(index) {
+            cars.splice(index, 1)
+        },
+        render() {
+            html = cars.map((car, index) => `
+                <li>
+                    ${car}
+                    <span class="delete" data-index=${index}>Delete</span>
+                </li>
+            `).join('')
+
+            root.innerHTML = html
+        },
+        handleDelete(e) {
+            const deleteBtn = e.target.closest('.delete')
+            if(deleteBtn) {
+                const index = deleteBtn.dataset.index
+                this.delete(index)
+                this.render()
+                console.log(index);
+            }
+        },
+        init() {
+            // Handle dom events
+            submit.onclick = () => {
+                const car = input.value
+                this.add(car)
+                this.render()
+                input.value = null
+                input.focus()
+            }
+
+            root.onclick = this.handleDelete.bind(this)
+
+            this.render()
+        }
+    }
+})();
+
+appCar.init()
